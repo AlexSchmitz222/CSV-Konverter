3816c89605c8ed8fdf2fd11b6aa2de48794e3f16
alexschmitz222 <alexschmitz222@gmail.com>
2021-11-04 23:00:02 +0100

    added functions for storing new events, removing a selected event and deleting all events

--- functions.py
+++ functions.py
@@ -1,21 +1,19 @@
 
 def exportCSVFile(filepath: str, entry_count: int, entry_list: list):
-    if (filepath==''):
+    if (filepath == ''):
         return
-    filewrite=open(filepath,"w")
-    filewrite.write("EVENT NAME,EVENT DESCRIPTION,START DATE,START TIME,END DATE,END TIME,ALL DAY EVENT?,CATEGORIES\n")
-    filewrite.close()
+    with open(filepath, "w") as filewrite:
+        filewrite.write(
+            "EVENT NAME,EVENT DESCRIPTION,START DATE,START TIME,END DATE,END TIME,ALL DAY EVENT?,CATEGORIES\n")
+    with open(filepath, "w") as filewrite:
+        for entry in range(entry_count):
+            for i in range(0, 8):
+                filewrite.write('\"' + str(entry_list[entry][i]) + '\"')
+                if i < 7:
+                    filewrite.write(",")
+            filewrite.write("\n")
 
-    filewrite=open(filepath,"a")
-    for entry in range(entry_count):
-        for i in range(0,8):
-            filewrite.write('\"' + entry_list[entry][i] + '\"')
-            if i < 7:
-                filewrite.write(",")
-        filewrite.write("\n")
-    filewrite.close()
 
- 
 def Update_Curr_Entry(cur_sel, entries):
     index = ([int(a) for a in cur_sel][0])
     str_eventname = entries[index][0]
@@ -26,12 +24,31 @@ def Update_Curr_Entry(cur_sel, entries):
     str_endtime = entries[index][5]
     bool_alldayevent = entries[index][6]
     str_categories = entries[index][7]
-    print(str_categories)
     return str_eventname, str_eventdescription, str_startdate, str_starttime, str_enddate, str_endtime, bool_alldayevent, str_categories
 
 
-def save_new_event(event_name: str, event_description: str, start_date: str, start_time: str, end_date: str, end_time: str, alldayevent: bool, categories):
-    if (event_name=='' or event_description=='' or start_date==''):
-        return
-    print(event_name + " , " + event_description + " , " + start_date + " , " + start_time + " , " + end_date + " , " + end_time + " , " + str(alldayevent) + " , " + categories)
+def save_new_event(event_name: str, event_description: str, start_date: str, start_time: str, end_date: str, end_time: str, alldayevent: bool, event_categories: tuple, entries: list, categories: list):
+    # if (event_name!=0 and event_description!=0 and start_date!=0 and start_time!=0 and end_date!=0 and end_time!=0 and alldayevent=='false' and categories!=0 or event_name!=0 and event_description!=0 and start_date!=0 and end_date!=0 and  alldayevent=='true' and categories!=0):
+    #print(event_name + " , " + event_description + " , " + start_date + " , " + start_time + " , " + end_date + " , " + end_time + " , " + str(alldayevent) + " , " + str(event_categories))
+    new_event = []
+    new_event_categories = []
+    new_event.extend([event_name, (event_description[: -1]),
+                     start_date, start_time, end_date, end_time, alldayevent])
+    for i in event_categories:
+        new_event_categories.append(categories[i])
+    new_event.append(new_event_categories)
+    print(new_event)
+    entries.append(new_event)
+    return(entries)
 
+
+def delete_curr_entry(cur_sel: tuple, entries: list):
+    if (len(entries) > 0):
+        index = ([int(a) for a in cur_sel][0])
+        entries.pop(index)
+        return entries
+
+
+def delete_all(entries: list):
+    entries.clear()
+    return entries
--- main.py
+++ main.py
@@ -2,72 +2,97 @@ from tkinter import *
 from tkinter import ttk
 from functions import *
 from tkinter import filedialog
-
+# -CHANGE HERE TO UPDATE AVAILABLE EVENT CATEGORIES------
 categories = ["Allgemein", "Eltern", "Schüler", "Unterrichtsfrei"]
 
-#-TKINTER EVENTS----------------------------------------
+
+entries = [["Test0", "Das ist ein Text BLABLABLA", "10-02-2021", "10:12:00", "10-02-2021", "10:15:00", "false", "Unterrichtsfrei, Eltern"],
+           ["Test1", "Das ist ein Text <br>BLABwadwLABLA</br>", "10-04-2021", "12:15:00", "10-06-2021", "10:30:00", "true", "Bratwurst, Oxford"]]
+
+# -TKINTER EVENTS----------------------------------------
 
 
 def event_listbox_select(event):
-    a, b, c, d, e, f, g, h = (Update_Curr_Entry(lstbx_event_list.curselection(), liste))
+    if (len(lstbx_event_list.curselection()) != 0):
 
+        a, b, c, d, e, f, g, h = (Update_Curr_Entry(
+            lstbx_event_list.curselection(), entries))
+
+        ent_event_name.delete(0, END)
+        ent_event_name.insert(0, str(a))
+
+        txtbx_event_description.delete(1.0, END)
+        txtbx_event_description.insert(1.0, str(b))
+
+        ent_start_date.delete(0, END)
+        ent_start_date.insert(0, str(c))
+
+        ent_start_time.delete(0, END)
+        ent_start_time.insert(0, str(d))
+
+        ent_end_date.delete(0, END)
+        ent_end_date.insert(0, str(e))
+
+        ent_end_time.delete(0, END)
+        ent_end_time.insert(0, str(f))
+
+        if (g == 'true'):
+            bool_alldayevent.set(1)
+        else:
+            bool_alldayevent.set(0)
+
+
+def event_export_entries():
+    exportCSVFile(filedialog.asksaveasfilename(title="Exportieren...", initialdir='~/Documents', defaultextension=".csv",
+                  filetype=(("Comma Separated Value", "*.csv"), ("All Files", "*.*"))), len(entries), entries)
+
+
+def event_new_entry():  # clear all input fields
     ent_event_name.delete(0, END)
-    ent_event_name.insert(0, str(a))
-
     txtbx_event_description.delete(1.0, END)
-    txtbx_event_description.insert(1.0, str(b))
-
     ent_start_date.delete(0, END)
-    ent_start_date.insert(0, str(c))
-
     ent_start_time.delete(0, END)
-    ent_start_time.insert(0, str(d))   
-
-    ent_end_date.delete(0, END)
-    ent_end_date.insert(0, str(e))
-
-    ent_end_time.delete(0, END)
-    ent_end_time.insert(0, str(f))
-
-    if (g == 'true'):
-        bool_alldayevent.set(1)
-    else:
-        bool_alldayevent.set(0)
-
-def export_entries():
-    exportCSVFile(filedialog.asksaveasfilename(title="Exportieren...", initialdir='~/Documents', defaultextension=".csv", filetype=(("Comma Separated Value", "*.csv"),("All Files", "*.*"))), len(liste), liste)
-
-def new_event(): #clear all input fields
-    ent_event_name.delete(0, END)
-    txtbx_event_description.delete(1.0, END)
-    ent_start_date.delete(0, END)
-    ent_start_time.delete(0, END)  
     ent_end_date.delete(0, END)
     ent_end_time.delete(0, END)
     bool_alldayevent.set(0)
 
-def save_event():
-    save_new_event(ent_event_name.get(), txtbx_event_description.get("1.0",END), ent_start_date.get(), ent_start_time.get(),ent_end_date.get(), ent_end_time.get(), bool_alldayevent.get(), lstbx_categories.get(ANCHOR))
 
-   
-
-def delete_event():
-    print()
-
-liste = [["Test0","Das ist ein Text <br>BLABLABLA</br>", "10-02-2021", "10:12:00", "10-02-2021", "10:15:00", "false", "Chinatown, Döner"], ["Test1","Das ist ein Text <br>BLABwadwLABLA</br>", "10-04-2021", "12:15:00", "10-06-2021", "10:30:00", "true", "Bratwurst, Oxford"]]
-#for i in range(2,25):
-#    liste.append(["Test" + str(i)])
+def event_save_entry():
+    global entries
+    entries = save_new_event(ent_event_name.get(), txtbx_event_description.get("1.0", END), ent_start_date.get(), ent_start_time.get(
+    ), ent_end_date.get(), ent_end_time.get(), bool_alldayevent.get(), lstbx_categories.curselection(), entries, categories)
+    lstbx_event_list.delete(0, END)
+    for i in range(0, len(entries)):
+        lstbx_event_list.insert(END, entries[i][0])
 
 
-#-MAIN PROGRAM------------------------------------------
+def event_delete_entry():
+    if (len(lstbx_event_list.curselection()) != 0):
+        global entries
+        entries = delete_curr_entry(lstbx_event_list.curselection(), entries)
+        lstbx_event_list.delete(0, END)
+        for i in range(0, len(entries)):
+            lstbx_event_list.insert(END, entries[i][0])
 
-bool_new_event='False' #if true, current event is new and hasn't been saved yet
+
+def event_delete_all():
+    global entries
+    entries = delete_all(entries)
+
+# for i in range(2,25):
+#    entries.append(["Test" + str(i)])
+
+
+# -MAIN PROGRAM------------------------------------------
+
+bool_new_event = 'False'  # if true, current event is new and hasn't been saved yet
 
 root = Tk()
 root.title("Kalender Tool")
 root.resizable(False, False)
 
-frm_curr_event = ttk.Frame(root, relief="groove", borderwidth=5, width=600, height=100)
+frm_curr_event = ttk.Frame(root, relief="groove",
+                           borderwidth=5, width=600, height=100)
 frm_curr_event.grid(column=1, row=1, sticky="N")
 
 lbl_event_name = ttk.Label(frm_curr_event, text="Name")
@@ -80,7 +105,8 @@ ent_event_name.grid(column=1, columnspan=2, row=2, sticky="W")
 lbl_event_description = ttk.Label(frm_curr_event, text="Beschreibung")
 lbl_event_description.grid(column=1, row=3, sticky="W")
 
-txtbx_event_description = Text(frm_curr_event, height=10, width=38, relief="raised")
+txtbx_event_description = Text(
+    frm_curr_event, height=10, width=38, relief="raised")
 txtbx_event_description.grid(column=1, columnspan=2, row=4, sticky="W")
 
 
@@ -111,55 +137,61 @@ lbl_end_time.grid(column=2, row=7, sticky="W")
 ent_end_time = ttk.Entry(frm_curr_event)
 ent_end_time.grid(column=2, row=8, sticky="W")
 
-bool_alldayevent=BooleanVar()
-chk_all_Day_event = ttk.Checkbutton(frm_curr_event, text="Ganztägiges Event?", variable=bool_alldayevent)
+bool_alldayevent = BooleanVar()
+chk_all_Day_event = ttk.Checkbutton(
+    frm_curr_event, text="Ganztägiges Event?", variable=bool_alldayevent)
 chk_all_Day_event.grid(column=1, row=9, sticky="W")
 
 lbl_categories = ttk.Label(frm_curr_event, text="Kategorien")
 lbl_categories.grid(column=1, row=10, sticky="W")
 
-categories_var=StringVar(value=[item for item in categories])
-lstbx_categories=Listbox(frm_curr_event, height=4, width=50, listvariable=categories_var, relief="raised", selectmode='multiple')
+categories_var = StringVar(value=[item for item in categories])
+lstbx_categories = Listbox(frm_curr_event, height=4, width=50,
+                           listvariable=categories_var, relief="raised", selectmode='multiple')
 lstbx_categories.grid(column=1, columnspan=2, row=11, sticky="W")
 
 
+frm_treelist = ttk.Frame(root, relief="groove",
+                         borderwidth=5, width=200, height=100)
+frm_treelist.grid(column=2, row=1, sticky="N")
 
 
-frm_treelist = ttk.Frame(root, relief="groove", borderwidth=5, width=200, height=100)
-frm_treelist.grid(column=2, row=1, sticky="N") 
+entries_var = StringVar(value=[item[0] for item in entries])
 
-
-
-entries_var=StringVar(value=[item[0] for item in liste])
-
-lstbx_event_list=Listbox(frm_treelist, height=25, width=31, listvariable=entries_var, relief="raised")
+lstbx_event_list = Listbox(frm_treelist, height=25,
+                           width=31, listvariable=entries_var, relief="raised")
 lstbx_event_list.pack()
 lstbx_event_list.bind('<<ListboxSelect>>', event_listbox_select)
-
-
 
 
 frm_entry_controls = ttk.Frame(relief="groove", borderwidth=5, width=200)
 frm_entry_controls.grid(column=3, row=1, sticky="N")
 
-btn_create_entry = ttk.Button(frm_entry_controls, text="Neues Event", width=20, command=new_event)
+btn_create_entry = ttk.Button(
+    frm_entry_controls, text="Neues Event", width=20, command=event_new_entry)
 btn_create_entry.pack()
-#btn_create_entry.state(['disabled']) 
+# btn_create_entry.state(['disabled'])
 
-btn_delete_entry = ttk.Button(frm_entry_controls, text="Event löschen", width=20, command=delete_event)
+btn_delete_entry = ttk.Button(
+    frm_entry_controls, text="Event löschen", width=20, command=event_delete_entry)
 btn_delete_entry.pack()
-#btn_delete_entry.state(['disabled']) 
+# btn_delete_entry.state(['disabled'])
 
-btn_save_entry = ttk.Button(frm_entry_controls, text="Event speichern", width=20, command=save_event)
+btn_save_entry = ttk.Button(
+    frm_entry_controls, text="Event speichern", width=20, command=event_save_entry)
 btn_save_entry.pack()
-#btn_save_entry.state(['disabled']) 
+# btn_save_entry.state(['disabled'])
 
+btn_delete_all = ttk.Button(
+    frm_entry_controls, text="Alles löschen", width=20, command=event_delete_all)
+btn_delete_all.pack()
 
 
 frm_controls = ttk.Frame(relief="groove", borderwidth=5, width=200)
-frm_controls.grid(column=3, row=1, sticky="S") 
+frm_controls.grid(column=3, row=1, sticky="S")
 
-btn_create_new_db = ttk.Button(frm_controls, text="Datenbank erstellen", width=20)
+btn_create_new_db = ttk.Button(
+    frm_controls, text="Datenbank erstellen", width=20)
 btn_create_new_db.pack()
 
 btn_load_from_db = ttk.Button(frm_controls, text="Datenbank laden", width=20)
@@ -167,19 +199,18 @@ btn_load_from_db.pack()
 
 btn_save_to_db = ttk.Button(frm_controls, text="Datenbank speichern", width=20)
 btn_save_to_db.pack()
-btn_save_to_db.state(['disabled']) 
+btn_save_to_db.state(['disabled'])
 
 btn_import = ttk.Button(frm_controls, text="Importieren", width=20)
 btn_import.pack()
 
-btn_export = ttk.Button(frm_controls, text="Exportieren", width=20, command=export_entries)
+btn_export = ttk.Button(frm_controls, text="Exportieren",
+                        width=20, command=event_export_entries)
 btn_export.pack()
-#btn_export.state(['disabled']) 
-
+# btn_export.state(['disabled'])
 
 
 root.mainloop()
 
 
-
-#exportCSVFile("C:/Users/alexs/Desktop/test.csv", 2, liste)
+#exportCSVFile("C:/Users/alexs/Desktop/test.csv", 2, entries)
